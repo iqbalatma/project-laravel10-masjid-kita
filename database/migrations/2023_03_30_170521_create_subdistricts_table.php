444dbd5e4c1b15e551a7aff7a4ff3a7302bd061d
iqbalatma <iqbalatma@gmail.com>
2023-03-31 00:18:11 +0700

    set migration and model done

--- database/migrations/2023_03_30_170521_create_subdistricts_table.php
+++ database/migrations/2023_03_30_170521_create_subdistricts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('subdistricts', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->unsignedBigInteger("district_id");
+            $table->softDeletes();
             $table->timestamps();
         });
     }
